5415f29a0f0f7a26d7110a7d1e41b18b8e7baa90
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-02-14 06:04:31 +0900

    [ADD] gateway 내의 GlobalFilter 작성

---
+++ gateway-server/src/main/java/com/velocia/gatewayserver/global/filter/GlobalFilter.java
@@ -0,0 +1,28 @@
+package com.velocia.gatewayserver.global.filter;
+
+import com.velocia.gatewayserver.global.data.FilterConfig;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
+public class GlobalFilter extends AbstractGatewayFilterFactory<FilterConfig> {
+    private static final Logger LOGGER = LogManager.getLogger(GlobalFilter.class);
+
+    public GlobalFilter() {
+        super(FilterConfig.class);
+    }
+
+    @Override
+    public GatewayFilter apply(FilterConfig config) {
+        return ((exchange, chain) -> {
+            LOGGER.info("GlobalFilter BASE | | | | -> " + config.getUsage());
+            LOGGER.info("GlobalFilter START | | | | -> " + exchange.getRequest());
+            return chain.filter(exchange).then(Mono.fromRunnable(() ->
+                    LOGGER.info("GlobalFilter END | | | | -> " + exchange.getResponse())));
+        });
+    }
+}

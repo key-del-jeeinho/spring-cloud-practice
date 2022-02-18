361d097bd302d25398a28fb1544ac09838f41d62
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-02-18 11:38:40 +0900

    [ADD] Eureka 클라이언트 구현

--- account-api/src/main/java/com/velocia/accountapi/AccountApiApplication.java
+++ account-api/src/main/java/com/velocia/accountapi/AccountApiApplication.java
@@ -2,7 +2,12 @@ package com.velocia.accountapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class AccountApiApplication {
 
@@ -10,4 +15,10 @@ public class AccountApiApplication {
         SpringApplication.run(AccountApiApplication.class, args);
     }
 
+    //Eureka 가 적용된 RestTemplate (Ribbon 은 non-blocking 기반의 WebClient 를 지원하지 않는다.)
+    @LoadBalanced @Bean
+    public RestTemplate commonRestTemplate() {
+        final RestTemplate restTemplate = new RestTemplate();
+        return restTemplate;
+    }
 }
